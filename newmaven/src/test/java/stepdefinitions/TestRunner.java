fa873a5200b9614a33ff28979bfcea6172fa0a3f
labuser <labuser@172.31.36.22>
2026-02-12 13:40:13 +0000

    commit

--- newmaven/src/test/java/stepdefinitions/TestRunner.java
+++ newmaven/src/test/java/stepdefinitions/TestRunner.java
@@ -3,7 +3,7 @@ package stepdefinitions;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @SuppressWarnings("deprecation")
